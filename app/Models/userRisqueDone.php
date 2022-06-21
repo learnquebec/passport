fcd4e378db8c0a8ffc428f1f727731a86b565f3c
martinclo <45047550+martinclo@users.noreply.github.com>
2022-06-21 11:43:31 -0400

    capitalize class name

--- app/Models/userRisqueDone.php
+++ app/Models/userRisqueDone.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class userRisqueDone extends Model
+class UserRisqueDone extends Model
 {
     use HasFactory;
 }
